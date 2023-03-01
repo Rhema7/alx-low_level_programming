32e5cc49a50dbf3a478483afe445729b6f78aeaa
Rhema7 <narteymichael029@gmail.com>
2023-02-28 17:19:22 -0800

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,7 @@
 /**
  * puts2 - function should print only one character out of two
  * starting with the first one
- * @str: output
+ * @str: input
  * Return: print
  */
 void puts2(char *str)
@@ -18,11 +18,11 @@ void puts2(char *str)
 		longi++;
 	}
 	t = longi - 1;
-	for (o = 0; o <=t; o++)
+	for (o = 0; o <= t; o++)
 	{
 		if (o % 2 == 0)
 	{
-		_putchar(str[0]);
+		_putchar(str[o]);
 	}
 	}
 	_putchar('\n');
--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,7 @@
 /**
  * print_array - a function that prints n elements of an array
  * @a: array name
- * @n: the number of elements of the array to be printed
+ * @n: is the number of elements of the array to be printed
  * Return: a and n inputs
  */
 void print_array(int *a, int n)
